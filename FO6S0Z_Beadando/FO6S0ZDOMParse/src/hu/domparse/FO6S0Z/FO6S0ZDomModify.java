917b449f0438d092bf927a67dd9be31a0f8a26e8
JapaneseGoblin <szovati.oliver123@gmail.com>
2025-12-06 12:57:09 +0100

    done

--- FO6S0Z_Beadando/FO6S0ZDOMParse/src/hu/domparse/FO6S0Z/FO6S0ZDomModify.java
+++ FO6S0Z_Beadando/FO6S0ZDOMParse/src/hu/domparse/FO6S0Z/FO6S0ZDomModify.java
@@ -8,12 +8,13 @@ import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.*;
+
 public class FO6S0ZDomModify {
 
-	public static void main(String[] args) {
-		try {
+    public static void main(String[] args) {
+        try {
             File inputFile = new File("FO6S0ZXML.xml");
-            
+
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
             Document doc = docBuilder.parse(inputFile);
@@ -23,10 +24,10 @@ public class FO6S0ZDomModify {
             // 1. MÓDOSÍTÁS: "Egri csillagok" Műfajának átírása
             System.out.println("1. Módosítás: K01 könyv műfajának pontosítása...");
             NodeList books = doc.getElementsByTagName("Könyv");
-            
+
             for (int i = 0; i < books.getLength(); i++) {
                 Element book = (Element) books.item(i);
-                
+
                 if ("K01".equals(book.getAttribute("K_ID"))) {
                     NodeList childNodes = book.getChildNodes();
                     for (int j = 0; j < childNodes.getLength(); j++) {
@@ -41,10 +42,10 @@ public class FO6S0ZDomModify {
             // 2. MÓDOSÍTÁS: P001 Példány állapotának javítása
             System.out.println("2. Módosítás: P001 példány állapotának javítása...");
             NodeList copies = doc.getElementsByTagName("Példány");
-            
+
             for (int i = 0; i < copies.getLength(); i++) {
                 Element copy = (Element) copies.item(i);
-                
+
                 if ("P001".equals(copy.getAttribute("P_ID"))) {
                     NodeList childNodes = copy.getChildNodes();
                     for (int j = 0; j < childNodes.getLength(); j++) {
@@ -59,32 +60,32 @@ public class FO6S0ZDomModify {
             // 3. MÓDOSÍTÁS: Szerző (SZ02) email címének cseréje
             System.out.println("3. Módosítás: SZ02 szerző email címének cseréje...");
             NodeList authors = doc.getElementsByTagName("Szerző");
-            
+
             for (int i = 0; i < authors.getLength(); i++) {
                 Element author = (Element) authors.item(i);
-                
+
                 if ("SZ02".equals(author.getAttribute("SZ_ID"))) {
-                     NodeList childNodes = author.getChildNodes();
-                     for (int j = 0; j < childNodes.getLength(); j++) {
-                         Node child = childNodes.item(j);
-                         if ("Email".equals(child.getNodeName())) {
-                             child.setTextContent("new.jkrowling@official.uk");
-                         }
-                     }
+                    NodeList childNodes = author.getChildNodes();
+                    for (int j = 0; j < childNodes.getLength(); j++) {
+                        Node child = childNodes.item(j);
+                        if ("Email".equals(child.getNodeName())) {
+                            child.setTextContent("new.jkrowling@official.uk");
+                        }
+                    }
                 }
             }
 
             // 4. MÓDOSÍTÁS: Kölcsönző (U01) utca nevének javítása (Beágyazott elem)
             System.out.println("4. Módosítás: U01 olvasó elköltözött (Utca módosítása)...");
             NodeList users = doc.getElementsByTagName("Kölcsönző");
-            
+
             for (int i = 0; i < users.getLength(); i++) {
                 Element user = (Element) users.item(i);
-                
+
                 if ("U01".equals(user.getAttribute("KÖL_ID"))) {
                     Node lakcimNode = user.getElementsByTagName("Lakcím").item(0);
                     NodeList lakcimChilds = lakcimNode.getChildNodes();
-                    
+
                     for (int j = 0; j < lakcimChilds.getLength(); j++) {
                         Node child = lakcimChilds.item(j);
                         if ("Utca".equals(child.getNodeName())) {
@@ -96,21 +97,21 @@ public class FO6S0ZDomModify {
 
             // Kimenet kiírása konzolra
             System.out.println("\n--- Módosított XML dokumentum tartalma: ---");
-            
+
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
-            
+
             transformer.setOutputProperty(OutputKeys.INDENT, "yes");
             transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
 
             DOMSource source = new DOMSource(doc);
             StreamResult consoleResult = new StreamResult(System.out);
-            
+
             transformer.transform(source, consoleResult);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-	}
+    }
 
 }
--- FO6S0Z_Beadando/FO6S0ZDOMParse/src/hu/domparse/FO6S0Z/FO6S0ZDomRead.java
+++ FO6S0Z_Beadando/FO6S0ZDOMParse/src/hu/domparse/FO6S0Z/FO6S0ZDomRead.java
@@ -1,158 +1,191 @@
 package hu.domparse.FO6S0Z;
 
-import java.io.*;
-import javax.xml.parsers.*;
-
+import java.io.File;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.*;
-
 
 public class FO6S0ZDomRead {
 
-	public static void main(String[] args) throws SAXException,
-    IOException, ParserConfigurationException{
-		File xmlFile = new File("FO6S0ZXML.xml");
+	public static void main(String[] args) {
+		try {
+			File xmlFile = new File("FO6S0ZXML.xml");
 
-        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder dBuilder = factory.newDocumentBuilder();
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder dBuilder = factory.newDocumentBuilder();
+			Document doc = dBuilder.parse(xmlFile);
 
-        Document doc = dBuilder.parse(xmlFile);
+			doc.getDocumentElement().normalize();
 
-        doc.getDocumentElement().normalize();
+			System.out.println("Gyökér elem: " + doc.getDocumentElement().getNodeName());
+			System.out.println("-------------------------");
 
-        System.out.println("Gyökér elem: " + doc.getDocumentElement().getNodeName());
-        System.out.println("-------------------------");
+			System.out.println("\n--- SZERZŐK ---");
+			NodeList nList = doc.getElementsByTagName("Szerző");
 
+			for (int i = 0; i < nList.getLength(); i++) {
+				Node nNode = nList.item(i);
 
-        System.out.println("\n--- SZERZŐK ---");
-        NodeList nList = doc.getElementsByTagName("Szerző");
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element elem = (Element) nNode;
 
-        for (int i = 0; i < nList.getLength(); i++) {
-            Node nNode = nList.item(i);
-            
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element elem = (Element) nNode;
+					String id = elem.getAttribute("SZ_ID");
+					String nev = elem.getElementsByTagName("Név").item(0).getTextContent();
+					String nemzetiseg = elem.getElementsByTagName("Nemzetiség").item(0).getTextContent();
+					String szulEv = elem.getElementsByTagName("SzületésiÉv").item(0).getTextContent();
+					String email = elem.getElementsByTagName("Email").item(0).getTextContent();
 
-                String id = elem.getAttribute("SZ_ID");
-                String nev = elem.getElementsByTagName("Név").item(0).getTextContent();
-                String nemzetiseg = elem.getElementsByTagName("Nemzetiség").item(0).getTextContent();
-                String szulEv = elem.getElementsByTagName("SzületésiÉv").item(0).getTextContent();
-                
-                System.out.println("Szerző ID: " + id);
-                System.out.println("Név: " + nev);
-                System.out.println("Nemzetiség: " + nemzetiseg);
-                System.out.println("Születési Év: " + szulEv);
-                System.out.println("-");
-            }
-        }
+					System.out.println("Szerző ID: " + id);
+					System.out.println("Név: " + nev);
+					System.out.println("Nemzetiség: " + nemzetiseg);
+					System.out.println("Születési Év: " + szulEv);
+					System.out.println("Email: " + email);
+					System.out.println("-");
+				}
+			}
 
-        // 2. KÖNYVEK BEOLVASÁSA
-        System.out.println("\n--- KÖNYVEK ---");
-        nList = doc.getElementsByTagName("Könyv");
+			System.out.println("\n--- KÖNYVEK ---");
+			nList = doc.getElementsByTagName("Könyv");
 
-        for (int i = 0; i < nList.getLength(); i++) {
-            Node nNode = nList.item(i);
+			for (int i = 0; i < nList.getLength(); i++) {
+				Node nNode = nList.item(i);
 
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element elem = (Element) nNode;
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element elem = (Element) nNode;
 
-                String id = elem.getAttribute("K_ID");
-                String cim = elem.getElementsByTagName("Cím").item(0).getTextContent();
-                String leiras = elem.getElementsByTagName("Leírás").item(0).getTextContent();
+					String id = elem.getAttribute("K_ID");
+					String cim = elem.getElementsByTagName("Cím").item(0).getTextContent();
+					String leiras = elem.getElementsByTagName("Leírás").item(0).getTextContent();
 
-                System.out.println("Könyv ID: " + id);
-                System.out.println("Cím: " + cim);
-                System.out.println("Leírás: " + leiras);
+					System.out.println("Könyv ID: " + id);
+					System.out.println("Cím: " + cim);
+					System.out.println("Leírás: " + leiras);
 
-                // MŰFAJ KEZELÉSE 
-                NodeList mufajList = elem.getElementsByTagName("Műfaj");
-                for (int j = 0; j < mufajList.getLength(); j++) {
-                    System.out.println("Műfaj: " + mufajList.item(j).getTextContent());
-                }
-                System.out.println("-");
-            }
-        }
+					NodeList mufajList = elem.getElementsByTagName("Műfaj");
+					for (int j = 0; j < mufajList.getLength(); j++) {
+						System.out.println("Műfaj: " + mufajList.item(j).getTextContent());
+					}
+					System.out.println("-");
+				}
+			}
 
-        // 3. PÉLDÁNYOK BEOLVASÁSA
-        System.out.println("\n--- PÉLDÁNYOK ---");
-        nList = doc.getElementsByTagName("Példány");
+			System.out.println("\n--- ÍRÓK KAPCSOLATOK ---");
+			nList = doc.getElementsByTagName("Kapcsolat");
 
-        for (int i = 0; i < nList.getLength(); i++) {
-            Node nNode = nList.item(i);
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element elem = (Element) nNode;
-                
-                String pid = elem.getAttribute("P_ID");
-                String kid = elem.getElementsByTagName("K_ID").item(0).getTextContent();
-                String ev = elem.getElementsByTagName("KiadásiÉv").item(0).getTextContent();
-                String allapot = elem.getElementsByTagName("Állapot").item(0).getTextContent();
+			for (int i = 0; i < nList.getLength(); i++) {
+				Node nNode = nList.item(i);
 
-                System.out.println("Példány ID: " + pid);
-                System.out.println("Tartozik a következő könyvhöz (K_ID): " + kid);
-                System.out.println("Kiadás éve: " + ev);
-                System.out.println("Állapot: " + allapot);
-                System.out.println("-");
-            }
-        }
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element elem = (Element) nNode;
+					String kid = elem.getElementsByTagName("K_ID").item(0).getTextContent();
+					String szid = elem.getElementsByTagName("SZ_ID").item(0).getTextContent();
 
-        // 4. KÖLCSÖNZŐK (OLVASÓK) BEOLVASÁSA
-        System.out.println("\n--- KÖLCSÖNZŐK ---");
-        nList = doc.getElementsByTagName("Kölcsönző");
+					System.out.println("Könyv ID: " + kid + " <-> Szerző ID: " + szid);
+				}
+			}
 
-        for (int i = 0; i < nList.getLength(); i++) {
-            Node nNode = nList.item(i);
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element elem = (Element) nNode;
+			System.out.println("\n--- PÉLDÁNYOK ---");
+			nList = doc.getElementsByTagName("Példány");
 
-                String kolId = elem.getAttribute("KÖL_ID");
-                String nev = elem.getElementsByTagName("Név").item(0).getTextContent();
-                String email = elem.getElementsByTagName("Email").item(0).getTextContent();
-                
-                // LAKCÍM (Komplex elem) KEZELÉSE
-                // Először lekérjük a Lakcím node-ot, majd abból az al-elemeket
-                Node lakcimNode = elem.getElementsByTagName("Lakcím").item(0);
-                String varos = "", utca = "", hazszam = "";
-                
-                if (lakcimNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element lakcimElem = (Element) lakcimNode;
-                    varos = lakcimElem.getElementsByTagName("Város").item(0).getTextContent();
-                    utca = lakcimElem.getElementsByTagName("Utca").item(0).getTextContent();
-                    hazszam = lakcimElem.getElementsByTagName("Házszám").item(0).getTextContent();
-                }
+			for (int i = 0; i < nList.getLength(); i++) {
+				Node nNode = nList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element elem = (Element) nNode;
 
-                System.out.println("Olvasó ID: " + kolId);
-                System.out.println("Név: " + nev);
-                System.out.println("Email: " + email);
-                System.out.println("Lakcím: " + varos + ", " + utca + " " + hazszam + ".");
-                System.out.println("-");
-            }
-        }
+					String pid = elem.getAttribute("P_ID");
+					String kid = elem.getElementsByTagName("K_ID").item(0).getTextContent();
+					String ev = elem.getElementsByTagName("KiadásiÉv").item(0).getTextContent();
+					String allapot = elem.getElementsByTagName("Állapot").item(0).getTextContent();
 
-        // 5. KÖLCSÖNZÉSEK (TRANZAKCIÓK) BEOLVASÁSA
-        System.out.println("\n--- KÖLCSÖNZÉSEK ---");
-        nList = doc.getElementsByTagName("Kölcsönzés");
+					System.out.println("Példány ID: " + pid);
+					System.out.println("Könyv Ref (K_ID): " + kid);
+					System.out.println("Kiadás éve: " + ev);
+					System.out.println("Állapot: " + allapot);
+					System.out.println("-");
+				}
+			}
 
-        for (int i = 0; i < nList.getLength(); i++) {
-            Node nNode = nList.item(i);
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element elem = (Element) nNode;
+			System.out.println("\n--- KÖLCSÖNZŐK ---");
+			nList = doc.getElementsByTagName("Kölcsönző");
 
-                String koId = elem.getAttribute("KO_ID");
-                String kolId = elem.getElementsByTagName("KÖL_ID").item(0).getTextContent();
-                String pId = elem.getElementsByTagName("P_ID").item(0).getTextContent();
-                String kezdet = elem.getElementsByTagName("Kezdete").item(0).getTextContent();
+			for (int i = 0; i < nList.getLength(); i++) {
+				Node nNode = nList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element elem = (Element) nNode;
 
-                System.out.println("Tranzakció ID: " + koId);
-                System.out.println("Olvasó (KÖL_ID): " + kolId);
-                System.out.println("Példány (P_ID): " + pId);
-                System.out.println("Dátum: " + kezdet);
-                System.out.println("-");
-            }
-        }
-		
+					String kolId = elem.getAttribute("KÖL_ID");
+					String nev = elem.getElementsByTagName("Név").item(0).getTextContent();
+					String email = elem.getElementsByTagName("Email").item(0).getTextContent();
+					String tel = elem.getElementsByTagName("Telefonszám").item(0).getTextContent();
+
+					Node lakcimNode = elem.getElementsByTagName("Lakcím").item(0);
+					String varos = "", utca = "", hazszam = "";
+
+					if (lakcimNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element lakcimElem = (Element) lakcimNode;
+						varos = lakcimElem.getElementsByTagName("Város").item(0).getTextContent();
+						utca = lakcimElem.getElementsByTagName("Utca").item(0).getTextContent();
+						hazszam = lakcimElem.getElementsByTagName("Házszám").item(0).getTextContent();
+					}
+
+					System.out.println("Olvasó ID: " + kolId);
+					System.out.println("Név: " + nev);
+					System.out.println("Email: " + email);
+					System.out.println("Tel: " + tel);
+					System.out.println("Lakcím: " + varos + ", " + utca + " " + hazszam);
+					System.out.println("-");
+				}
+			}
+
+			System.out.println("\n--- BÉRLETEK ---");
+			nList = doc.getElementsByTagName("Bérlet");
+
+			for (int i = 0; i < nList.getLength(); i++) {
+				Node nNode = nList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element elem = (Element) nNode;
+
+					String bid = elem.getAttribute("B_ID");
+					String vonalkod = elem.getElementsByTagName("Vonalkód").item(0).getTextContent();
+					String kiallitva = elem.getElementsByTagName("Kiállítva").item(0).getTextContent();
+					String vege = elem.getElementsByTagName("Vége").item(0).getTextContent();
+					String kolId = elem.getElementsByTagName("KÖL_ID").item(0).getTextContent();
+
+					System.out.println("Bérlet ID: " + bid);
+					System.out.println("Vonalkód: " + vonalkod);
+					System.out.println("Tulajdonos (KÖL_ID): " + kolId);
+					System.out.println("Érvényesség: " + kiallitva + " - " + vege);
+					System.out.println("-");
+				}
+			}
+
+			System.out.println("\n--- KÖLCSÖNZÉSEK ---");
+			nList = doc.getElementsByTagName("Kölcsönzés");
+
+			for (int i = 0; i < nList.getLength(); i++) {
+				Node nNode = nList.item(i);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element elem = (Element) nNode;
+
+					String koId = elem.getAttribute("KO_ID");
+					String kolId = elem.getElementsByTagName("KÖL_ID").item(0).getTextContent();
+					String pId = elem.getElementsByTagName("P_ID").item(0).getTextContent();
+					String kezdet = elem.getElementsByTagName("Kezdete").item(0).getTextContent();
+					String veg = elem.getElementsByTagName("Vége").item(0).getTextContent();
+
+					System.out.println("Tranzakció ID: " + koId);
+					System.out.println("Olvasó: " + kolId + " -> Példány: " + pId);
+					System.out.println("Időszak: " + kezdet + " - " + veg);
+					System.out.println("-");
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
